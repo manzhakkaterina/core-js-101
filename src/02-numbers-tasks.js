19b518449cdf5af912bf7356b0436e29c03ba975
Katerina Manzhak <59136296+manzhakkaterina@users.noreply.github.com>
2023-05-30 23:40:08 +0300

    Update 02-numbers-tasks.js

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -18,8 +18,8 @@
  *   5, 10 => 50
  *   5, 5  => 25
  */
-function getRectangleArea(width, height) {
-  return width * height;
+function getRectangleArea(/* width, height */) {
+  throw new Error('Not implemented');
 }
 
 /**
@@ -33,8 +33,8 @@ function getRectangleArea(width, height) {
  *   3.14 => 19.729201864543903
  *   0    => 0
  */
-function getCircleCircumference(radius) {
-  return 2 * Math.PI * radius;
+function getCircleCircumference(/* radius */) {
+  throw new Error('Not implemented');
 }
 
 /**
@@ -49,8 +49,8 @@ function getCircleCircumference(radius) {
  *  10, 0  => 5
  *  -3, 3  => 0
  */
-function getAverage(value1, value2) {
-  return (value1 / 2) + (value2 / 2);
+function getAverage(/* value1, value2 */) {
+  throw new Error('Not implemented');
 }
 
 /**
@@ -68,8 +68,8 @@ function getAverage(value1, value2) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(x1, y1, x2, y2) {
-  return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
+  throw new Error('Not implemented');
 }
 
 /**
@@ -84,8 +84,8 @@ function getDistanceBetweenPoints(x1, y1, x2, y2) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot(a, b) {
-  return (-b / a);
+function getLinearEquationRoot(/* a, b */) {
+  throw new Error('Not implemented');
 }
 
 /**
@@ -106,10 +106,8 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(x1, y1, x2, y2) {
-  const prom1 = Math.sqrt(x1 * x1 + y1 * y1);
-  const prom2 = Math.sqrt(x2 * x2 + y2 * y2);
-  return Math.acos((x1 * x2 + y1 * y2) / (prom1 * prom2));
+function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
+  throw new Error('Not implemented');
 }
 
 /**
@@ -124,9 +122,8 @@ function getAngleBetweenVectors(x1, y1, x2, y2) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(value) {
-  const str = value.toString();
-  return str[str.length - 1];
+function getLastDigit(/* value */) {
+  throw new Error('Not implemented');
 }
 
 /**
@@ -140,8 +137,8 @@ function getLastDigit(value) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(value) {
-  return Number(value);
+function parseNumberFromString(/* value */) {
+  throw new Error('Not implemented');
 }
 
 /**
@@ -157,8 +154,8 @@ function parseNumberFromString(value) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(a, b, c) {
-  return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
+function getParallelepipedDiagonal(/* a, b, c */) {
+  throw new Error('Not implemented');
 }
 
 /**
@@ -178,8 +175,8 @@ function getParallelepipedDiagonal(a, b, c) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(num, pow) {
-  return Math.round(num / (10 ** pow)) * (10 ** pow);
+function roundToPowerOfTen(/* num, pow */) {
+  throw new Error('Not implemented');
 }
 
 /**
@@ -199,13 +196,8 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(n) {
-  if (n === 2 || n === 3) return true;
-  if (n <= 1 || n % 2 === 0 || n % 3 === 0) return false;
-  for (let i = 5; i * i <= n; i = i + 6) {
-    if (n % i === 0 || n % (i + 2) === 0) return false;
-  }
-  return true;
+function isPrime(/* n */) {
+  throw new Error('Not implemented');
 }
 
 /**
@@ -223,11 +215,8 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(value, def) {
-  if (Number.isNaN(+value)) {
-    return def;
-  }
-  return Number(value);
+function toNumber(/* value, def */) {
+  throw new Error('Not implemented');
 }
 
 module.exports = {
@@ -244,4 +233,3 @@ module.exports = {
   isPrime,
   toNumber,
 };
-
